7651954507e217507f487d2d46c69224a4a1b982
Raspredval <rustKravets@gmail.com>
2026-07-02 10:43:35 +0700

    minor naming changes, for consistency

--- include/Patterns.hpp
+++ include/Patterns.hpp
@@ -11,8 +11,8 @@ static_assert(__cplusplus >= 202506, "requires C++26 minimum version");
 #include "Match.hpp"
 #include "FixedString.hpp"
 
-#define IMP_DECL(name) static imp::Match name (FILE* f, imp::CapturesList& c, const std::any& u)
-#define IMP_PROXY(name, fn) IMP_DECL(name) { return (fn)(f, c, u); }
+#define IMP_DECL(name) static imp::Match name (FILE* f, imp::CapturesList& g, const std::any& u)
+#define IMP_PROXY(name, fn) IMP_DECL(name) { return (fn)(f, g, u); }
 
 namespace imp {
     using Captures =
