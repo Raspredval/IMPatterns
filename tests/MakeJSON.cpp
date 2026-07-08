512359ffd05a7ca0793739f3901ccc44a606c30f
Raspredval <rustKravets@gmail.com>
2026-07-08 13:13:27 +0700

    updated the json generator

--- tests/MakeJSON.cpp
+++ tests/MakeJSON.cpp
@@ -2,8 +2,6 @@
 #include <cstdio>
 #include <memory>
 
-#include <unistd.h>
-
 using CFile = std::unique_ptr<FILE,
     decltype([] (FILE* hFile) { if (hFile) fclose(hFile); })>;
 
@@ -11,13 +9,8 @@ constexpr const char*
     szFilename  = "./assets/test.json";
 
 int main() {
-    if (access(szFilename, F_OK) == 0) {
-        printf("%s already exists\n", szFilename);
-        return EXIT_SUCCESS;
-    }
-
     CFile
-        uptrJSON    = CFile{fopen64("./assets/test.json", "wb")};
+        uptrJSON    = CFile{fopen64("./assets/test.json", "w")};
     if (!uptrJSON) {
         fprintf(stderr, "failed to open %s\n", szFilename);
         return EXIT_FAILURE;
@@ -29,9 +22,10 @@ int main() {
             "Pretty",
             "Cheesy",
             "Glorious",
+
             "Smelly",
             "Fearless",
-            "Lil"
+            "Lil",
             "L33t"
         };
     const char*
@@ -40,20 +34,21 @@ int main() {
             "pig",
             "dude",
             "girl",
+
             "car",
             "mousey",
             "ninja",
             "vim enjoyer"
         };
 
+    srand(time(nullptr));
     fprintf(uptrJSON.get(), "{\n\t\"scores\": [\n");
-    for (size_t i = 0; i != 1'000'000'000; ++i) {
-        int iRnd    = rand();
-        fprintf(uptrJSON.get(), "\t\t{ \"%s %s\": %f },\n",
-            lpPrefixes[iRnd % 8], lpPostfixes[iRnd % 8],
-            (double)iRnd / (double)RAND_MAX);
+    for (size_t i = 0; i != 5'000'000; ++i) {
+        fprintf(uptrJSON.get(), "\t\t{ \"%s %s\": %d },\n",
+            lpPrefixes[rand() % 8], lpPostfixes[rand() % 8], rand() % 100 + 1);
     }
-    fprintf(uptrJSON.get(), "\t]\n}");
+    fprintf(uptrJSON.get(), "\t\t{ \"%s %s\": %d }\n\t]\n}",
+        lpPrefixes[rand() % 8], lpPostfixes[rand() % 8], rand() % 100 + 1);
 
     return EXIT_SUCCESS;
 }
