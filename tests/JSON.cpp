9f1ae2f416970136fc65158abbb790784e4eaa50
Raspredval <rustKravets@gmail.com>
2026-07-31 21:19:12 +0700

    cosmetic changes

--- tests/JSON.cpp
+++ tests/JSON.cpp
@@ -3,26 +3,25 @@
 #include <Patterns.hpp>
 
 namespace grammJSON {
-    IMP_DECL_PATTERN(spacing);
-    IMP_DECL_PATTERN(object);
-    IMP_DECL_PATTERN(array);
-    IMP_DECL_PATTERN(field);
-    IMP_DECL_PATTERN(value);
-    IMP_DECL_PATTERN(boolean);
-    IMP_DECL_PATTERN(null);
-    IMP_DECL_PATTERN(string);
-    IMP_DECL_PATTERN(strfill);
-    IMP_DECL_PATTERN(escseq);
-    IMP_DECL_PATTERN(number);
-    IMP_DECL_PATTERN(numint);
-    IMP_DECL_PATTERN(numfract);
-    IMP_DECL_PATTERN(numexp);
+    IMP_DECL_PATTERN(static spacing);
+    IMP_DECL_PATTERN(static object);
+    IMP_DECL_PATTERN(static array);
+    IMP_DECL_PATTERN(static field);
+    IMP_DECL_PATTERN(static value);
+    IMP_DECL_PATTERN(static boolean);
+    IMP_DECL_PATTERN(static null);
+    IMP_DECL_PATTERN(static string);
+    IMP_DECL_PATTERN(static strfill);
+    IMP_DECL_PATTERN(static escseq);
+    IMP_DECL_PATTERN(static number);
+    IMP_DECL_PATTERN(static numint);
+    IMP_DECL_PATTERN(static numfract);
 
-    IMP_MAKE_PATTERN(spacing, (
+    IMP_MAKE_PATTERN(spacing,
         imp::AtLeast<0>(imp::SpaceOrNewLine())
-    ))
+    )
 
-    IMP_MAKE_PATTERN(object, (
+    IMP_MAKE_PATTERN(object,
         imp::Str<"{">() >> imp::Fn<spacing>() >>
         imp::UpTo<1>(
             imp::Fn<field>() >> imp::Fn<spacing>() >>
@@ -31,9 +30,9 @@ namespace grammJSON {
                 imp::Fn<field>() >> imp::Fn<spacing>()
             )
         ) >> imp::Str<"}">()
-    ))
+    )
 
-    IMP_MAKE_PATTERN(array, (
+    IMP_MAKE_PATTERN(array,
         imp::Str<"[">() >> imp::Fn<spacing>() >>
         imp::UpTo<1>(
             imp::Fn<value>() >> imp::Fn<spacing>() >>
@@ -42,74 +41,73 @@ namespace grammJSON {
                 imp::Fn<value>() >> imp::Fn<spacing>()
             )
         ) >> imp::Str<"]">()
-    ))
+    )
 
-    IMP_MAKE_PATTERN(field, (
+    IMP_MAKE_PATTERN(field,
         imp::Fn<string>() >> imp::Fn<spacing>() >>
         imp::Str<":">() >> imp::Fn<spacing>() >>
         imp::Fn<value>()
-    ))
+    )
 
-    IMP_MAKE_PATTERN(value, (
+    IMP_MAKE_PATTERN(value,
         imp::Fn<object>()  | imp::Fn<array>()  |
         imp::Fn<string>()  | imp::Fn<number>() |
         imp::Fn<boolean>() | imp::Fn<null>()
-    ))
+    )
 
-    IMP_MAKE_PATTERN(boolean, (
+    IMP_MAKE_PATTERN(boolean,
         imp::Str<"true">() | imp::Str<"false">()
-    ))
+    )
 
-    IMP_MAKE_PATTERN(null, (
+    IMP_MAKE_PATTERN(null,
         imp::Str<"null">()
-    ))
+    )
 
-    IMP_MAKE_PATTERN(string, (
+    IMP_MAKE_PATTERN(string,
         imp::Str<"\"">() >> imp::Fn<strfill>() >>
         imp::AtLeast<0>(
             imp::Fn<escseq>() >> imp::Fn<strfill>()
         ) >> imp::Str<"\"">()
-    ))
+    )
 
-    IMP_MAKE_PATTERN(strfill, (
+    IMP_MAKE_PATTERN(strfill,
         imp::AtLeast<0>(imp::NegSet<"\"\\">())
-    ))
+    )
 
-    IMP_MAKE_PATTERN(escseq, (
+    IMP_MAKE_PATTERN(escseq,
         imp::Str<"\\">() >> (
             imp::Set<"/\"\\bfnrt">() |
             imp::Set<"uU">() >> imp::Exactly<4>(imp::HexDigit())
         )
-    ))
+    )
 
-    IMP_MAKE_PATTERN(number, (
+    IMP_MAKE_PATTERN(number,
         imp::Fn<numint>() >> imp::UpTo<1>(imp::Fn<numfract>())
-    ))
+    )
 
-    IMP_MAKE_PATTERN(numint, (
+    IMP_MAKE_PATTERN(numint,
         imp::UpTo<1>(imp::Set<"+-">()) >> imp::AtLeast<1>(imp::Digit())
-    ))
+    )
 
-    IMP_MAKE_PATTERN(numfract, (
+    IMP_MAKE_PATTERN(numfract,
         imp::Str<".">() >> imp::AtLeast<1>(imp::Digit()) >>
         imp::UpTo<1>(
             imp::Set<"eE">() >> imp::Fn<numint>()
         )
-    ))
+    )
 
-    IMP_MAKE_PATTERN(eval, (
-        (
-            imp::Fn<spacing>() >> imp::UpTo<1>(
-                imp::Fn<value>() >> imp::Fn<spacing>()
-            ) >> imp::None()
-        ) / [] (FILE* hFile, const imp::Match& m, imp::CapturesView, const std::any&) -> imp::Match {
+    IMP_MAKE_PATTERN(eval,
+        (imp::Fn<spacing>() >> imp::UpTo<1>(
+            imp::Fn<value>() >> imp::Fn<spacing>()
+        ) >> imp::None()) /
+        [] (FILE* hFile, const imp::Match& m, imp::CapturesView, const std::any&) -> imp::Match {
             if (!m)
                 fprintf(stderr, "failed to parse JSON at %zi\n", ftell(hFile));
             else
                 printf("success\n");
             return m;
         }
-    ))
+    )
 }
 
 using CFile = std::unique_ptr<FILE,
