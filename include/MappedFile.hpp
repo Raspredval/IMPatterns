8e1baf773788f3ddaaa1b122010d4637720df990
Raspredval <rustKravets@gmail.com>
2026-08-11 19:23:35 +0700

    refactored the memory mapping portion, added a stub windows implementation, NOT TESTED, HAVENT EVEN TRIED TO COMPILE, NEED TO CHECK WITH MINGW

--- include/MappedFile.hpp
+++ include/MappedFile.hpp
@@ -1,88 +1,188 @@
 #pragma once
 #include <span>
+#include <cstdint>
 #include <string_view>
 
+#ifdef _WIN32
+#include <windows.h>
+#else
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#endif
 
 namespace imp {
-    class MappedFile {
-    public:
-        inline
-        MappedFile(std::string_view strvFilename) noexcept {
-            struct stat
-                file_info;
-            void*
-                lpMapped;
+    namespace __impl {
+        #ifdef _WIN32
+        struct mmap_windows {
+            static inline std::span<const char>
+            map_file(std::string_view strvFilename) noexcept {
+                void*
+                    lpFileView  = nullptr;
+                uint64_t
+                    uFileSize   = 0;
+                BY_HANDLE_FILE_INFORMATION
+                    file_info   = {};
+                std::span<const char>
+                    spnResult   = {};
+                HANDLE
+                    hFile       = INVALID_HANDLE_VALUE;
+                HANDLE
+                    hFileMap    = NULL;
 
-            int
-                fdMapped    = open(strvFilename.data(), O_RDONLY);
-            if (fdMapped < 0 || fstat(fdMapped, &file_info) < 0)
-                goto finally;
+                hFile           = CreateFileA(
+                                    strvFilename.data(),
+                                    GENERIC_READ,
+                                    0, nullptr,
+                                    OPEN_EXISTING, 0);
+                if (hFile == INVALID_HANDLE_VALUE)
+                    goto finally;
 
-            lpMapped        = mmap(
-                                NULL, (size_t)file_info.st_size,
-                                PROT_READ, MAP_PRIVATE,
-                                fdMapped, 0);
-            if (lpMapped == MAP_FAILED)
-                goto finally;
+                if (!GetFileInformationByHandle(hFile, &file_info))
+                    goto finally;
+                uFileSize   = (uint64_t)file_info.nFileSizeLow |
+                                (uint64_t)file_info.nFileSizeHigh << 32;
 
-            this->spnData   = {
-                (const char*)lpMapped,
-                (size_t)file_info.st_size
-            };
+                hFileMap    = CreateFileMappingA(
+                                hFile, nullptr,
+                                PAGE_READONLY,
+                                (uint32_t)(uFileSize >> 32),
+                                (uint32_t)uFileSize,
+                                nullptr);
+                if (hFileMap == NULL)
+                    goto finally;
 
-        finally:
-            if (fdMapped > 0)
-                close(fdMapped);
-        }
+                lpFileView  = MapViewOfFile(
+                                hFileMap,
+                                FILE_MAP_READ,
+                                0, 0,
+                                uFileSize);
+                if (lpFileView == NULL)
+                    goto finally;
 
-        inline
-        ~MappedFile() noexcept {
-            if (this->spnData.data()) {
-                munmap(
-                    (void*)this->spnData.data(),
-                    this->spnData.size());
+                spnFileInfo = {
+                    (const char*)lpFileView,
+                    file_info.
+                }
+
+            finally:
+                if (hFile != INVALID_HANDLE_VALUE)
+                    CloseHandle(hFile);
+                if (hFileMap != NULL)
+                    CloseHandle(hFileMap);
+
+                return spnResult;
             }
-        }
 
-        MappedFile(const MappedFile&) = delete;
-        MappedFile&
-        operator=(const MappedFile&) = delete;
+            static inline bool
+            unmap_file(std::span<const char> spnFileView) noexcept {
+                return UnmapViewOfFile((LPCVOID)spnFileView.data());
+            }
+        };
+        #else
+        struct mmap_posix {
+            static inline std::span<const char>
+            map_file(std::string_view strvFilename) noexcept {
+                void*
+                    lpFileView  = nullptr;
+                uint64_t
+                    uFileSize   = 0;
+                struct stat
+                    file_info   = {};
+                std::span<const char>
+                    spnResult   = {};
+                int
+                    hFile       = -1;
 
-        inline
-        MappedFile(MappedFile&& obj) noexcept {
-            this->spnData   = obj.spnData;
-            obj.spnData     = {};
-        }
+                hFile           = open(strvFilename.data(), O_RDONLY);
+                if (hFile < 0)
+                    goto finally;
 
-        inline MappedFile&
-        operator=(MappedFile&& obj) noexcept {
-            MappedFile
-                temp    = std::move(obj);
-            std::swap(temp.spnData, this->spnData);
-            return *this;
-        }
+                if (fstat(hFile, &file_info) < 0)
+                    goto finally;
+                uFileSize       = (size_t)file_info.st_size;
 
-        inline std::span<const char>
-        Range() const noexcept {
-            return this->spnData;
-        }
+                lpFileView      = mmap(
+                                    NULL, uFileSize,
+                                    PROT_READ, MAP_PRIVATE,
+                                    hFile, 0);
+                if (lpFileView == MAP_FAILED)
+                    goto finally;
 
-        inline bool
-        Bad() const noexcept {
-            return this->spnData.data() == nullptr;
-        }
+                spnResult       = {
+                    (const char*)lpFileView, uFileSize
+                };
 
-        inline
-        operator bool() const noexcept {
-            return !this->Bad();
-        }
+            finally:
+                if (hFile > 0)
+                    close(hFile);
 
-    private:
-        std::span<const char>
-            spnData;
-    };
+                return spnResult;
+            }
+
+            static inline bool
+            unmap_file(std::span<const char> spnFileView) noexcept {
+                return !munmap((void*)spnFileView.data(), spnFileView.size());
+            }
+        };
+        #endif
+
+        template<typename impl>
+        class MappedFile {
+        public:
+            inline
+            MappedFile(std::string_view strvFilename) noexcept {
+                this->spnData   = impl::map_file(strvFilename);
+            }
+
+            inline
+            ~MappedFile() noexcept {
+                impl::unmap_file(this->spnData);
+            }
+
+            MappedFile(const MappedFile&) = delete;
+            MappedFile&
+            operator=(const MappedFile&) = delete;
+
+            inline
+            MappedFile(MappedFile&& obj) noexcept {
+                this->spnData   = obj.spnData;
+                obj.spnData     = {};
+            }
+
+            inline MappedFile&
+            operator=(MappedFile&& obj) noexcept {
+                MappedFile
+                    temp    = std::move(obj);
+                std::swap(temp.spnData, this->spnData);
+                return *this;
+            }
+
+            inline std::span<const char>
+            Range() const noexcept {
+                return this->spnData;
+            }
+
+            inline bool
+            Bad() const noexcept {
+                return this->spnData.data() == nullptr;
+            }
+
+            inline
+            operator bool() const noexcept {
+                return !this->Bad();
+            }
+
+        private:
+            std::span<const char>
+                spnData;
+        };
+    }
+
+    #ifdef _WIN32
+    using MappedFile = __impl::MappedFile<__impl::mmap_windows>;
+    #else
+    using MappedFile = __impl::MappedFile<__impl::mmap_posix>;
+    #endif
 }
